47c754a62690cd16519d8d876b83a7f8250dbae4
Leemihi <dlalgml0203@kaywon.ac.kr>
2024-03-25 15:29:15 +0900

    물리엔진 & 캐릭터 움직임 스크립트

---
+++ Assets/UnityTechnologies/Scripts/PlayerMove.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Vector3 movement;
+    private Animator animator;
+    private Rigidbody rb;
+    private Quaternion rotation = Quaternion.identity;
+
+    // HeaderAttribute : 변수에 설명을 넣을 때 사용
+    [HeaderAttribute("회전 속도")]
+    public float turnSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Getcomponent : 컴퍼넌트를 불러옴
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // 가로축, 세로축 입력
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // 벡터 입력
+        movement.Set(horizontal, 0f, vertical);
+       
+        // 정규화
+        movement.Normalize();
+
+        // Mathf.Approximately : 근사값 (입력값, 원하는 값)
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        animator.SetBool("IsWalking", isWalking);
+
+        // desiredFoward : 바라볼 방향 계산
+        Vector3 desiredFoward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.fixedDeltaTime, 0f);
+        // Quaternion.LookRotation : 벡터방향으로 바라봄
+        rotation = Quaternion.LookRotation(desiredFoward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        // rb.MovePositio : 위치 이동
+        // rb.position : 현재 위치,
+        // movement : 입력 받아 움직이는 벡터
+        // animator.deltaPosition.magnitude : 애니메이션에서 움직이는 양
+        rb.MovePosition(rb.position + movement * animator.deltaPosition.magnitude);
+        rb.MoveRotation(rotation);
+    }
+}
